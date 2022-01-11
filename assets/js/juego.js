1a00d893c53e41e825f5281570b3562c728fc312
Fernanda Gastiarena <mf.gastiarena@gmail.com>
2022-01-11 12:16:09 -0300

    Creada baraja de cartas

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -6,3 +6,27 @@
  */
 
 let deck = []; //mazo de cartas
+const tiposCartas = ['C', 'D', 'H', 'S'];
+const cartasEspeciales = ['A', 'J', 'Q', 'K'];
+
+
+const crearDeck = () => {
+    for (let i = 2; i <= 10; i++) {
+        for (let tipo of tiposCartas) {
+            deck.push(i + tipo);
+        }
+    }
+
+    for (let tipo of tiposCartas) {
+        for (let esp of cartasEspeciales) {
+            deck.push(esp + tipo);
+        }
+    }
+
+    console.log(deck);
+    deck = _.shuffle(deck);
+    console.log(deck);
+    return deck;
+}
+
+crearDeck();
